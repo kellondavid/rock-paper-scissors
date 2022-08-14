6d7304783ea8a4bbeb956111830020d31abdce01
Kellon Ko <kellondavidko@gmail.com>
2022-08-13 22:15:56 -0700

    add 5 rounds, display winner of 5 rounds

--- index.js
+++ index.js
@@ -1,29 +1,54 @@
-let playerSelection = prompt("Choose rock, paper, or scissors.");
-    playerSelection = playerSelection.toLowerCase();
+function getPlayerChoice() {
+    let response = prompt("Choose rock, paper, or scissors.");
+        response = response.toLowerCase();
+    return response;
+}
     
 function getComputerChoice() {
     const rockPaperScissors = ["rock", "paper", "scissors"];
     return rockPaperScissors[Math.floor(Math.random() * rockPaperScissors.length)];
 }
-const computerSelection = getComputerChoice();
 
-function playRound(computerSelection, playerSelection) {
-    if (playerSelection === computerSelection) {
+
+let computerPoint = 0;
+let playerPoint = 0;
+
+function playRound() {
+    let playerSelection = getPlayerChoice();
+    let computerSelection = getComputerChoice();
+    if (playerSelection === computerSelection)  {
         return ("It's a tie! You both picked " + playerSelection + ".");
     } else if (playerSelection === "rock" && computerSelection === "scissors") {
+        playerPoint = ++playerPoint;
         return ("You won! Rock beats scissors!")
     } else if (playerSelection === "paper" && computerSelection === "rock") {
+        playerPoint = ++playerPoint;
         return ("You won! Paper beats rock!")
     } else if (playerSelection === "scissors" && computerSelection === "paper") {
+        playerPoint = ++playerPoint;
         return ("You won! Scissors beat paper!");
     } else {
+        computerPoint = ++computerPoint;
         return ("You lost! The computer chose " + computerSelection + ", which beats " + playerSelection + ".");
-    }
+    } 
 }
-console.log(playRound(computerSelection, playerSelection))
 
-function game(playRound) {
+
+function game() {
     for (let i = 0; i < 5; i++) {
-       
+     console.log(playRound())
     }
 }
+
+function winner() {
+    if (playerPoint === computerPoint) {
+        return ("It's a draw!");
+    } else if (playerPoint > computerPoint) {
+        return ("You beat the computer out of 5 rounds!");
+    } else if (computerPoint > playerPoint) {
+        return ("You lost to the computer out of 5 rounds!");
+    }
+}
+
+game();
+console.log(winner());
